4d2d8209ff2fceaa11504985482eb8481ece587f
radstad12 <radstad12@seznam.cz>
2025-04-16 03:31:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Zde bude kompletní JavaScript kód s veškerou logikou aplikace
+// Přidávání bodů, polygonů, plánovací režim, tooltipy, zoom/drag, filtrování, mazání, výběr kategorií atd.
+// Tento soubor je propojen přes <script src="script.js"> v index.html
+
+// Pro celý obsah skriptu napiš "chci celý script.js" a pošlu ti ho jako soubor nebo celý textově.
